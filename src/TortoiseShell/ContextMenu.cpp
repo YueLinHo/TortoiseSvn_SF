YueLinHo/TortoiseSvn_SF

commit f4f89c521ff25db7a1c4f72b4c8b860a33efe7cf

Fix a crash in the shell extension when copying unversioned files with Ctrl-C/Ctrl-V: * avoid accessing an illegal iterator.
git-svn-id: svn://svn.code.sf.net/p/tortoisesvn/code/trunk@9136 154be670-7969-47af-9ab0-c6fb378d2295

src/TortoiseShell/ContextMenu.cpp
@@ -482,7 +482,7 @@ STDMETHODIMP CShellExt::Initialize(LPCITEMIDLIST pIDFolder,
 	if ((files_.size() == 1)&&(m_State != FileStateDropHandler))
 	{
 		itemStates |= ITEMIS_ONLYONE;
-		if (PathIsDirectory(files_.front().c_str()))
+		if ((folder_.size())&&(PathIsDirectory(files_.front().c_str())))
 		{
 			folder_ = files_.front();
 			svn_wc_status_kind status = svn_wc_status_unversioned;
@@ -1942,6 +1942,8 @@ void CShellExt::InsertIgnoreSubmenus(UINT &idCmd, UINT idCmdFirst, BOOL ownerdra
 	bool bShowIgnoreMenu = false;
 	TCHAR maskbuf[MAX_PATH];		// MAX_PATH is ok, since this only holds a filename
 	TCHAR ignorepath[MAX_PATH];		// MAX_PATH is ok, since this only holds a filename
+	if (files_.size() == 0)
+		return;
 	std::vector<stdstring>::iterator I = files_.begin();
 	if (_tcsrchr(I->c_str(), '\\'))
 		_tcscpy_s(ignorepath, MAX_PATH, _tcsrchr(I->c_str(), '\\')+1);

commit 007125f895ed7df025da5214e01f4c9aefbc8d6a

Fix regression introduced in r9136: The folder_ vector is allowed to be empty.
git-svn-id: svn://svn.code.sf.net/p/tortoisesvn/code/trunk@9165 154be670-7969-47af-9ab0-c6fb378d2295

src/TortoiseShell/ContextMenu.cpp
@@ -486,7 +486,7 @@ STDMETHODIMP CShellExt::Initialize(LPCITEMIDLIST pIDFolder,
 	if ((files_.size() == 1)&&(m_State != FileStateDropHandler))
 	{
 		itemStates |= ITEMIS_ONLYONE;
-		if ((folder_.size())&&(PathIsDirectory(files_.front().c_str())))
+		if (PathIsDirectory(files_.front().c_str()))
 		{
 			folder_ = files_.front();
 			svn_wc_status_kind status = svn_wc_status_unversioned;