YueLinHo/TortoiseSvn_SF

commit 3ee2e395eb901017349f4a875591d2e130994315

* always block the full working copy if possible * wait 20ms after sending the block command to give the cache time to process it (Fixes issue #198) : Overlay lost on revert
git-svn-id: svn://svn.code.sf.net/p/tortoisesvn/code/trunk@22505 154be670-7969-47af-9ab0-c6fb378d2295

src/TSVNCache/SVNStatusCache.cpp
@@ -299,6 +299,21 @@ bool CSVNStatusCache::BlockPath(const CTSVNPath& path, DWORD timeout /* = 0 */)
 
     timeout = GetTickCount() + (timeout * 1000);    // timeout is in seconds, but we need the milliseconds
 
+    CTSVNPath p(path);
+    do
+    {
+        CTSVNPath dbPath(p);
+        dbPath.AppendPathString(g_SVNAdminDir.GetAdminDirName() + _T("\\wc.db"));
+        if (!dbPath.Exists())
+            p = p.GetContainingDirectory();
+        else
+        {
+            AutoLocker lock(m_NoWatchPathCritSec);
+            m_NoWatchPaths[p] = timeout;
+            return true;
+        }
+    } while (!p.IsEmpty());
+
     AutoLocker lock(m_NoWatchPathCritSec);
     m_NoWatchPaths[path.GetDirectory()] = timeout;
 
@@ -308,6 +323,27 @@ bool CSVNStatusCache::BlockPath(const CTSVNPath& path, DWORD timeout /* = 0 */)
 bool CSVNStatusCache::UnBlockPath(const CTSVNPath& path)
 {
     bool ret = false;
+
+    CTSVNPath p(path);
+    do
+    {
+        CTSVNPath dbPath(p);
+        dbPath.AppendPathString(g_SVNAdminDir.GetAdminDirName() + _T("\\wc.db"));
+        if (!dbPath.Exists())
+            p = p.GetContainingDirectory();
+        else
+        {
+            AutoLocker lock(m_NoWatchPathCritSec);
+            std::map<CTSVNPath, DWORD>::iterator it = m_NoWatchPaths.find(p);
+            if (it != m_NoWatchPaths.end())
+            {
+                CTraceToOutputDebugString::Instance()(_T(__FUNCTION__) _T(": path removed from no good: %s\n"), it->first.GetWinPath());
+                m_NoWatchPaths.erase(it);
+                ret = true;
+            }
+        }
+    } while (!p.IsEmpty());
+
     AutoLocker lock(m_NoWatchPathCritSec);
     std::map<CTSVNPath, DWORD>::iterator it = m_NoWatchPaths.find(path.GetDirectory());
     if (it != m_NoWatchPaths.end())

src/TSVNCache/CacheInterface.cpp
@@ -144,6 +144,11 @@ CBlockCacheForPath::CBlockCacheForPath(const WCHAR * aPath)
     path[MAX_PATH] = 0;
 
     SendCacheCommand (TSVNCACHECOMMAND_BLOCK, path);
+    // Wait a short while to make sure the cache has
+    // processed this command. Without this, we risk
+    // executing the svn command before the cache has
+    // blocked the path and already gets change notifications.
+    Sleep(20);
 }
 
 CBlockCacheForPath::~CBlockCacheForPath()

commit 183bb8e37f2473a4be2da82322dc521edaa1a30f

Fix missing break: without this we get into an endless loop. Problem was introduced in r22505.
git-svn-id: svn://svn.code.sf.net/p/tortoisesvn/code/trunk@22507 154be670-7969-47af-9ab0-c6fb378d2295

src/TSVNCache/SVNStatusCache.cpp
@@ -341,6 +341,7 @@ bool CSVNStatusCache::UnBlockPath(const CTSVNPath& path)
                 m_NoWatchPaths.erase(it);
                 ret = true;
             }
+            break;
         }
     } while (!p.IsEmpty());
 